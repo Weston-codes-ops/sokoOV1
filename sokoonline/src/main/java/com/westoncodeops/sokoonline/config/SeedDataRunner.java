9ccdac9deaf350d9cc3480b2e7a4da2f922ddec8
JaxCoreWes <srealleak@gmail.com>
2026-09-12 18:17:55 +0300

    Configure production deployment

--- sokoonline/src/main/java/com/westoncodeops/sokoonline/config/SeedDataRunner.java
+++ sokoonline/src/main/java/com/westoncodeops/sokoonline/config/SeedDataRunner.java
@@ -9,11 +9,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Profile("dev")
 public class SeedDataRunner implements CommandLineRunner {
 
     private final UserRepository userRepository;
